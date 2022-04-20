97077380414328d342b3d9959f48f7466a39d5da
RennanCarlos <104074121+RennanCarlos@users.noreply.github.com>
2022-04-20 09:33:11 -0300

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    
+    public static void main(String args[]){
+        Scanner rece = new Scanner(System.in);
+        //Puxa os nomes dos jogadores
+        String jogador1, jogador2;
+
+        System.out.print("Nome do jogador 1: ");
+        jogador1 = rece.nextLine();
+
+        System.out.print("Nome do jogador 1: ");
+        jogador2 = rece.nextLine();
+
+        
+    }
+}
